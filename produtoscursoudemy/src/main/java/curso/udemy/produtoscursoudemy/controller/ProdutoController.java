f9964c643c285a55a8ce8abdb4ec7bb3cab942cd
MatheusAlvesSilvaCode <matheusalvesdev@outlook.com>
2025-09-19 00:03:03 +0100

    find iproduct by id

--- produtoscursoudemy/src/main/java/curso/udemy/produtoscursoudemy/controller/ProdutoController.java
+++ produtoscursoudemy/src/main/java/curso/udemy/produtoscursoudemy/controller/ProdutoController.java
@@ -2,17 +2,39 @@ package curso.udemy.produtoscursoudemy.controller;
 
 
 import curso.udemy.produtoscursoudemy.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import curso.udemy.produtoscursoudemy.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RequestMapping("produtos")
 @RestController
 
 public class ProdutoController {
-    @PostMapping
-    public void salvar(@RequestBody Produto produto){
-        System.out.println("Produto Recebido: " + produto);
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
     }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto Recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+
+    }
+
+    @GetMapping("/{id}")
+     public Produto obterPorId(@PathVariable("id") String id){
+         Optional<Produto> produto = produtoRepository.findById(id);
+         return produto.isPresent() ? produto.get() : null; // Temos o produto? se sim pegue get() se não null
+     }
 }
---
+++ produtoscursoudemy/src/main/java/curso/udemy/produtoscursoudemy/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package curso.udemy.produtoscursoudemy.repository;
+
+import curso.udemy.produtoscursoudemy.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+}
